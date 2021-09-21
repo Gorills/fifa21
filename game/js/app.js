278247576cbfecb40f6f0ee993e955c488c79c31
Gorills <gorivanickiy@gmail.com>
2021-09-22 05:04:09 +0700

    footer

--- game/js/app.js
+++ game/js/app.js
@@ -165,3 +165,18 @@ function parallax(e){
     });
     
  
+
+
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        /*Если сделали скролл на 100px задаём новый класс для header*/
+        if(height > 20){
+            $('.mobile-footer').addClass('mobile-footer--active');
+           
+            
+        } else{
+            /*Если меньше 100px удаляем класс для header*/
+            $('.mobile-footer').removeClass('mobile-footer--active');
+            
+        }
+    });
